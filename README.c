df489b5a55bf54ea09e87586ce9982548e6039b1
ddeepikaa <ddeepikaa@users.noreply.github.com>
2016-07-08 14:58:40 +0530

    finish

---
+++ README.c
@@ -0,0 +1,15 @@
+# boop
+#include<stdio.h>
+int main()
+{
+  int num1,num2,num3,max;
+  max=num1;
+  if(max>num2)
+  {
+    max=num2;
+    if(max>num3)
+     max=num3;
+  }
+  printf("largest number%d",max);
+
+}
